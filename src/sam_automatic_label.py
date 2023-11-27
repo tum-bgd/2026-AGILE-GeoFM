bce02f52cffc8530eb6d805bc8b8ffabd35c61d7
Wejdene Mansour <wejdene.mansour@tum.de>
2023-11-27 08:35:39 +0100

    other tasks to explore

---
+++ src/sam_automatic_label.py
@@ -0,0 +1,2 @@
+# label all the masks returned by automatic SAM that is not prompted and only 
+# keep the ones that state building (maybe roof? since satellite view)
---
+++ src/sam_dino_prompt.py
@@ -0,0 +1,2 @@
+# use grounding DINO object detector to get bb based on text prompt of "building"
+# then use SAM prompted with these bb to segment
